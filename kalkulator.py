59477142526b207fb001f91f05211601c10f1ca7
Thoriq <thoriqmumtaz12612@gmail.com>
2026-03-12 14:29:52 +0700

    [add] menambahkan fitur/mode pemuaian

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,6 @@
 import os
 def clear():
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls")
 
 
 #kalkulator standar(aritmatika)
@@ -16,24 +16,29 @@ def Kalkulator_standar():
             no2 = float(input("masukkan angka: "))
             if op == "+":
                 hasil = (no1 + no2)
-                print(f"{no1} + {no2} = {hasil}")
+                print(f"\n{no1} + {no2} = {hasil}")
             elif op == "-":
                 hasil = (no1 - no2)
-                print(f"{no1} - {no2} = {hasil}")
+                print(f"\n{no1} - {no2} = {hasil}")
             elif op in ["/", ":"]:
                 hasil = (no1 / no2)
-                print(f"{no1} / {no2} = {hasil}")
+                print(f"\n{no1} / {no2} = {hasil}")
             elif op in ["x", "*"]:
                 hasil = (no1 * no2)
-                print(f"{no1} x {no2} = {hasil}")
+                print(f"\n{no1} x {no2} = {hasil}")
             else:
                 print("operator tidak ditemukan")
                 input("tekan Enter untuk melanjutkan...")
+                continue
                     
         except ValueError:
                 print("tolong masukkan angka!!")
+                input("tekan Enter untuk melanjutkan...")
+                continue
         except ZeroDivisionError:
                 print("Tidak dapat dibagi nol")
+                input("tekan Enter untuk melanjutkan...")
+                continue
 
         #mengakhiri while loop
         print("\n1. lanjut")
@@ -122,9 +127,141 @@ def Kalkulator_suhu():
         if out in ["kembali", "2", "2. kembali", "out"]:
             break
         else:
-            pass  
+            pass
 
-#menu awal, mengambil input
+#pemuaian panjang (function)
+def pemuaian_panjang():
+    while True:
+        clear()
+        print("\n---------- pemuaian panjang ----------\n")
+        try:
+            #input user
+            L0 = float(input("Masukkan panjang awal: "))
+            alpha = float(input("Masukkan koefisien muai panjang: "))
+            dT = float(input("Masukkan perubahan suhu: "))
+            
+            #menghitung input user
+            dL = alpha * L0 * dT
+            L_akhir = L0 + dL
+            
+            #menampilkan hasil kalkulasi
+            print("")
+            print(f"Pertambahan panjang = {dL}")
+            print(f"Panjang akhir = {L_akhir}")
+            
+            #mengakhiri loop
+            print("\n1. lanjut")
+            print("2. kembali")
+            out = input(">>> ").lower().strip()
+            if out in ["kembali", "2", "2. kembali", "out"]:
+                menu_pemuaian()
+            else:
+                pass
+        #mencegah Eror    
+        except ValueError:
+            print("tolong masukkan angka!!")
+            input("tekan enter untuk melanjutkan...")
+            continue
+        
+#pemuaian luas (function)       
+def pemuaian_luas():
+    while True:
+        clear()
+        print("\n---------- Pemuaian Luas ----------\n")
+        try:
+            #input user
+            A0 = float(input("Masukkan luas awal: "))
+            beta = float(input("Masukkan koefisien muai luas: "))
+            dT = float(input("Masukkan perubahan suhu: "))
+            
+            #menghitung input user
+            dA = beta * A0 * dT
+            A_akhir = A0 + dA
+
+            #menampilkan hasil kalkulasi
+            print("")
+            print(f"Pertambahan luas = {dA}")
+            print(f"Luas akhir = {A_akhir}")
+            
+            #mengakhiri loop
+            print("\n1. lanjut")
+            print("2. kembali")
+            out = input(">>> ").lower().strip()
+            if out in ["kembali", "2", "2. kembali", "out"]:
+                menu_pemuaian()
+            else:
+                pass
+
+        #mencegah Eror
+        except ValueError:
+            print("tolong masukkan angka!!")
+            input("tekan enter untuk melanjutkan...")
+            continue
+
+#pemuaian volume (function)
+def pemuaian_volume():
+    while True:
+        clear()
+        print("\n---------- Pemuaian Volume ----------\n")
+        try:
+            #input user
+            V0 = float(input("Masukkan volume awal: "))
+            gamma = float(input("Masukkan koefisien muai volume: "))
+            dT = float(input("Masukkan perubahan suhu: "))
+
+            #menghitung input user
+            dV = gamma * V0 * dT
+            V_akhir = V0 + dV
+
+            #menampilkan kalkulasi
+            print(f"Pertambahan volume = {dV}")
+            print(f"Volume akhir = {V_akhir}")
+            
+            #mengakhiri loop
+            print("")
+            print("\n1. lanjut")
+            print("2. kembali")
+            out = input(">>> ").lower().strip()
+            if out in ["kembali", "2", "2. kembali", "out"]:
+                menu_pemuaian()
+            else:
+                pass
+        
+        #mencegah Eror
+        except ValueError:
+            print("tolong masukkan angka!!")
+            input("tekan enter untuk melanjutkan...")
+            continue
+
+#menu kalkulator pemuaian, mengambil input untuk memilih mode
+def menu_pemuaian():
+    while True:
+        clear()
+
+        #mengambil input user, memilih mode
+        print("\n---------- kalkulator pemuaian ----------\n")
+        print("1. pemuaian panjang")
+        print("2. pemuaian luas")
+        print("3. pemuaian volume")
+        print("4. Kembali")
+        pilihan = input("Pilih menu: ")
+
+        #pemanggil function
+        if pilihan in ["1", "pemuaian panjang", "1. pemuaian panjang"]:
+            pemuaian_panjang()
+        elif pilihan in ["2", "pemuaian luas", "2. pemuaian luas"]:
+            pemuaian_luas()
+        elif pilihan in ["3", "pemuaian volume", "3. pemuaian volume"]:
+            pemuaian_volume()
+        elif pilihan in ["4", "kembali", "4. kembali"]:
+            kalkulator()
+        #mencegah Eror
+        else:
+            print("pilihan tidak valid")
+            input("tekan enter untuk melanjutkan...")
+            continue
+
+#menu awal kalkulator, mengambil input untuk memilih mode
 def kalkulator():
     while True:
         clear()
@@ -132,15 +269,18 @@ def kalkulator():
         print("pilih menu")
         print("--> 1.standar")
         print("--> 2.konversi suhu")
+        print("--> 3.pemuaian suhu")
         print("\nkeluar [X]")
         print("")
         mode = input("menu: ").lower().strip()
 
-        #pemangil fungsi
+        #pemangil function
         if mode in ["standar", "1", "1.standar"]:
             Kalkulator_standar()
         elif mode in ["konversi suhu", "2", "2.konversi suhu"]:
             Kalkulator_suhu()
+        elif mode in ["pemuaian suhu", "3", "3.pemuaian suhu"]:
+            menu_pemuaian()
         elif mode in ["x", "keluar"]:
             print("program selesai...")
             return
